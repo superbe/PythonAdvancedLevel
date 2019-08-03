4aff81063b1f671d22da0860744f71fbbf3426c8
Бабарыкин Евгений <babarykin@list.ru>
2019-08-04 04:59:59 +0500

    Немного переработал клиентский скрипт

--- Lessons/echo/client.py
+++ Lessons/echo/client.py
@@ -6,45 +6,50 @@ import client_log_config
 from argparse import ArgumentParser
 from datetime import datetime
 
-parser = ArgumentParser()
 
-parser.add_argument('-c', '--config', type=str, required=False, help='Sets config file path')
+def main():
+    parser = ArgumentParser()
 
-args = parser.parse_args()
+    parser.add_argument('-c', '--config', type=str, required=False, help='Sets config file path')
 
-config = {
-    'host': 'localhost',
-    'port': 8000,
-    'buffersize': 1024
-}
+    args = parser.parse_args()
 
-if args.config:
-    with open(args.config) as file:
-        file_config = yaml.load(file, Loader=yaml.Loader)
-        config.update(file_config)
-
-host, port = config.get('host'), config.get('port')
-
-try:
-    logging.info('Client was started')
-    sock = socket.socket()
-    sock.connect((host, port))
-
-    action = input('Enter action: ')
-    data = input('Enter data: ')
-
-    request = {
-        'action': action,
-        'time': datetime.now().timestamp(),
-        'data': data
+    config = {
+        'host': 'localhost',
+        'port': 8000,
+        'buffersize': 1024
     }
 
-    str_request = json.dumps(request)
+    if args.config:
+        with open(args.config) as file:
+            file_config = yaml.load(file, Loader=yaml.Loader)
+            config.update(file_config)
 
-    sock.send(str_request.encode())
-    logging.info(f'Client send data "{data}"')
+    host, port = config.get('host'), config.get('port')
 
-    b_response = sock.recv(config.get('buffersize')).decode();
-    logging.info(f'Server send data "{b_response}"')
-except KeyboardInterrupt:
-    logging.critical('client shutdown.')
+    try:
+        logging.info('Client was started')
+        sock = socket.socket()
+        sock.connect((host, port))
+
+        action = input('Enter action: ')
+        data = input('Enter data: ')
+
+        request = {
+            'action': action,
+            'time': datetime.now().timestamp(),
+            'data': data
+        }
+
+        str_request = json.dumps(request)
+
+        sock.send(str_request.encode())
+        logging.info(f'Client send data "{data}"')
+
+        b_response = sock.recv(config.get('buffersize')).decode();
+        logging.info(f'Server send data "{b_response}"')
+    except KeyboardInterrupt:
+        logging.critical('client shutdown.')
+
+
+main()
