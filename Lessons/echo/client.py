4a362d1b3e68261fed1ba4a8c6c4e4d60d3ce916
Бабарыкин Евгений <babarykin@list.ru>
2019-08-05 01:52:22 +0500

    Небольшой рефакторинг. Вынес конфигурирование программы в отдельный класс.

--- Lessons/echo/client.py
+++ Lessons/echo/client.py
@@ -1,15 +1,10 @@
-import yaml
 import socket
 import json
 import logging
 import client_log_config
 import zlib
-from argparse import ArgumentParser
+from client_configuration import configuration, WRITE_MODE, READ_MODE
 from datetime import datetime
-
-WRITE_MODE = 'write'
-
-READ_MODE = 'read'
 
 
 def make_request(action_name, data):
@@ -21,45 +16,21 @@ def make_request(action_name, data):
 
 
 def main():
-    parser = ArgumentParser()
-
-    parser.add_argument('-c', '--config', type=str, required=False, help='Sets config file path')
-
-    parser.add_argument('-m', '--mode', type=str, default='write', help='Sets client mode')
-
-    args = parser.parse_args()
-
-    config = {
-        'host': 'localhost',
-        'port': 8000,
-        'buffersize': 1024
-    }
-
-    if args.config:
-        with open(args.config) as file:
-            file_config = yaml.load(file, Loader=yaml.Loader)
-            config.update(file_config)
-
-    host, port = config.get('host'), config.get('port')
-
     try:
         logging.info('Client was started')
         sock = socket.socket()
-        sock.connect((host, port))
-
+        sock.connect((configuration.host, configuration.port))
         while True:
-            if args.mode == WRITE_MODE:
+            if configuration.mode == WRITE_MODE:
                 action = input('Enter action: ')
                 data = input('Enter data: ')
-
                 request = make_request(action, data)
                 str_request = json.dumps(request)
                 bytes_request = zlib.compress(str_request.encode())
-
                 sock.send(bytes_request)
-                logging.info(f'Client send data "{data}"')
-            elif args.mode == READ_MODE:
-                response = sock.recv(config.get('buffersize'));
+                # logging.info(f'\nClient send data "{data}"')
+            elif configuration.mode == READ_MODE:
+                response = sock.recv(configuration.bufferSize);
                 bytes_response = zlib.decompress(response)
                 logging.info(f'Server send data "{bytes_response.decode()}"')
     except KeyboardInterrupt:
--- Lessons/echo/server/__main__.py
+++ Lessons/echo/server/__main__.py
@@ -1,48 +1,23 @@
-import yaml
 import socket
-import json
 import logging
 import select
 import server_log_config
-from argparse import ArgumentParser
-
-# from server import actions
-from protocol import validate_request, make_response
-from actions import resolve
 from handlers import handle_default_request
+from server_configuration import configuration
 
 
 def main():
-    parser = ArgumentParser()
-
-    parser.add_argument('-c', '--config', type=str, required=False, help='Sets config file path')
-
-    args = parser.parse_args()
-
-    config = {
-        'host': 'localhost',
-        'port': 8000,
-        'buffersize': 1024
-    }
-
-    if args.config:
-        with open(args.config) as file:
-            file_config = yaml.load(file, Loader=yaml.Loader)
-            config.update(file_config)
-
     connections = []
     requests = []
 
-    host, port = config.get('host'), config.get('port')
-
     try:
         sock = socket.socket()
-        sock.bind((host, port))
+        sock.bind((configuration.host, configuration.port))
         sock.setblocking(False)
         sock.settimeout(0)
         sock.listen(5)
 
-        logging.info(f'Server started with {host}:{port}')
+        logging.info(f'Server started with {configuration.host}:{configuration.port}')
 
         while True:
             try:
@@ -56,7 +31,7 @@ def main():
                 rlist, wlist, xlist = select.select(connections, connections, connections, 0)
 
                 for read_client in rlist:
-                    bytes_request = read_client.recv(config.get('buffersize'))
+                    bytes_request = read_client.recv(configuration.bufferSize)
                     requests.append(bytes_request)
 
                 if requests:
